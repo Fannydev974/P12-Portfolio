0c1c438068d683d3f9c535d384f19f0a1c8351ec
Fanny Hadji <devfanny91@outlook.com>
2024-02-23 18:33:32 +0100

    Ajout mes travaux mais a changer pour technologies utilisés

--- src/components/Travaux/travaux.jsx
+++ src/components/Travaux/travaux.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import './travaux.css';
+import Html from '../../img/Html.png';
+import Css from '../../img/Css.png';
+import JavaScript from '../../img/icon JS.png';
+import IconReact from '../../img/icon React.png';
+import Sass from '../../img/icon sass.png';
 
 
 const travaux = () => {
@@ -12,6 +17,30 @@ const travaux = () => {
 
                 <button className='button s-button'>Télécharger mon cv</button>
                 <div className='fondViolet' style={{ background: '#ABF1FF94' }}></div>
+            </div>
+
+            {/* right */}
+            <div className='w-right'>
+                <div className='w-mainCircle'>
+                    <div className='w-secCircle'>
+                        <img src={Html} alt='' />
+                    </div>
+                    <div className='w-secCircle'>
+                        <img src={Css} alt='' />
+                    </div>
+                    <div className='w-secCircle'>
+                        <img src={JavaScript} alt='' />
+                    </div>{""}
+                    <div className='w-secCircle'>
+                        <img src={IconReact} alt='' />
+                    </div>
+                    <div className='w-secCircle'>
+                        <img src={Sass} alt='' />
+                    </div>
+                </div>
+                {/* Background Circles */}
+                <div className='w-backCircle pinkCircle'></div>
+                <div className='w-backCircle yellowCircle'></div>
             </div>
         </div>
     )
