bf77d1b68ed71dd12b9fef13fd2db7eaf1342b9d
Fanny Hadji <devfanny91@outlook.com>
2025-01-02 01:38:58 +0100

    ajout images modal

--- src/components/Portfolio/portfolio.jsx
+++ src/components/Portfolio/portfolio.jsx
@@ -14,6 +14,15 @@ import Printit from '../../img/printit.png';
 import ArrowLeft from '../../img/flecheroseGauche.webp';
 import ArrowRight from '../../img/flecherose.png'
 
+import BookiModal from '../../img/bookiModal.webp';
+import ModalOhmyfood from '../../img/Modal-ohmyfood1.webp';
+import ModalPrintit from '../../img//Modal-printit.png';
+import ModalSophie from '../../img/ModalSophie.webp';
+import ModalArgent from '../../img/ModalArgent.webp'
+import ModalNina from '../../img/ModalNina.webp';
+import ModalKasa from '../../img/ModalKasa.webp';
+import Modal724 from '../../img/Modal724.webp';
+
 function Portfolio() {
     const [modalOpen, setModalOpen] = useState(false);
     const [selectedImage, setSelectedImage] = useState(null);
@@ -53,7 +62,7 @@ function Portfolio() {
         return (
             <div className="modal-content">
                 <div>{description}</div>
-                {/*<button className='lienGithub' onClick={handleGithubClick}>Lien Github</button>*/}
+                {<button className='lienGithub' onClick={handleGithubClick}>Lien Github</button>}
             </div>
         );
     }
@@ -104,12 +113,12 @@ function Portfolio() {
                     {/*<img src={ArrowLeft} alt='Flèche de gauche'></img>
                     <img src={ArrowRight} alt='Flèche de droite'></img>*/}
                     <SwiperSlide>
-                        <img src={Booki} alt='Photo du site Booki' onClick={() => openModal(Booki, (
+                        <img src={Booki} alt='Photo du site Booki' onClick={() => openModal(BookiModal, (
                             <p>
-                                <strong>Client :</strong>
+                                <strong>Client : </strong>
                                 L'Entreprise souhaite développer un site web pour la recherche d'hébergements et d'activités.
-                                <br></br><strong>Présentation :</strong> Booki est un projet web qui vise à simplifier la recherche d'hébergements et d'activités dans la ville de choix de l'utilisateur. Mon rôle en tant que développeuse Front-End était de prendre les maquettes fournies par l'UI designer, et de les intégrer en utilisant HTML et CSS.
-                                <strong>Objectifs :</strong> Le défi pour cette mission était de rendre le projet responsif. Pour cela j'ai utilisé flexbox, cela a permis de pouvoir créer une expérience maximale pour les utilisateurs ainsi qu'une mise en page qui s'adapte à toutes sortes de taille d'écran différents (mobile, tablette, ordianteur).
+                                <br></br><strong>Présentation : </strong> Booki est un projet web qui vise à simplifier la recherche d'hébergements et d'activités dans la ville de choix de l'utilisateur. Mon rôle en tant que développeuse Front-End était de prendre les maquettes fournies par l'UI designer, et de les intégrer en utilisant HTML et CSS.<br></br>
+                                <strong>Objectifs : </strong> Le défi pour cette mission était de rendre le projet responsif. Pour cela j'ai utilisé flexbox, cela a permis de pouvoir créer une expérience maximale pour les utilisateurs ainsi qu'une mise en page qui s'adapte à toutes sortes de taille d'écran différents (mobile, tablette, ordianteur).<br></br>
                                 <strong>Pour cela j'ai utilisée: "HTML", "CSS", "Responsive".</strong>
                             </p>
                         ), 'https://github.com/Fannydev974/Booki')
@@ -117,100 +126,100 @@ function Portfolio() {
                     </SwiperSlide>
 
                     <SwiperSlide>
-                        <img src={Ohmyfood} alt='Photo du site Ohmyfood' onClick={() => openModal(Ohmyfood, (
+                        <img src={Ohmyfood} alt='Photo du site Ohmyfood' onClick={() => openModal(ModalOhmyfood, (
                             <p>
-                                <strong>Client :</strong>
+                                <strong>Client : </strong>
                                 OhMyFood est une Startup de restauration.
                                 <br></br>
-                                <strong>Présentation :</strong> OhMyFood souhaite développer un site répertoriant les menus de restaurants gastronomiques parisien. Mon rôle en tant que développeuse était de créer le site en respectant les maquettes fournies par l'équipe UX designer.
-                                <strong>Objectifs :</strong> L'objectif est de développer le site OhMyFood en "Mobile First" et de le dynamiser avec des animations CSS en utilisant le préprocesseur Sass. De plus, il fallais permettre la réservation en ligne et la composition de menus.
+                                <strong>Présentation : </strong> OhMyFood souhaite développer un site répertoriant les menus de restaurants gastronomiques parisien. Mon rôle en tant que développeuse était de créer le site en respectant les maquettes fournies par l'équipe UX designer.<br></br>
+                                <strong>Objectifs : </strong> L'objectif est de développer le site OhMyFood en "Mobile First" et de le dynamiser avec des animations CSS en utilisant le préprocesseur Sass. De plus, il fallais permettre la réservation en ligne et la composition de menus.<br></br>
                                 <strong>Pour cela j'ai utilisée: "HTML", "CSS", "Responsive".</strong>
                             </p>
                         ), 'https://github.com/Fannydev974/projetohmyfood')} />
                     </SwiperSlide>
                     <SwiperSlide>
-                        <img src={Printit} alt='Photo du site Print-it' onClick={() => openModal(Printit, (
+                        <img src={Printit} alt='Photo du site Print-it' onClick={() => openModal(ModalPrintit, (
                             <p>
-                                <strong>Client :</strong>
+                                <strong>Client : </strong>
                                 Print-it imprimerie familiale
                                 <br></br>
-                                <strong>Présentation :</strong>
-                                dynamiser le site Internet statique d’une petite imprimerie familiale nommée Print it.
-                                <strong>Objectif :</strong>
-                                dynamiser le site Internet statique d’une petite imprimerie familiale, en ajoutant a celui-ci un carrousel d'images qui défile avec des images chisies à l'avance par le gérant de cette imprimerie.
+                                <strong>Présentation : </strong>
+                                Dynamiser le site Internet statique d’une petite imprimerie familiale nommée Print it.<br></br>
+                                <strong>Objectif : </strong>
+                                Dynamiser le site Internet statique d’une petite imprimerie familiale, en ajoutant a celui-ci un carrousel d'images qui défile avec des images chisies à l'avance par le gérant de cette imprimerie.<br></br>
                                 <strong>Pour cela j'ai utilisée: "JavaScript", "DOM".</strong>
                             </p>
                         ), 'https://github.com/Fannydev974/Print-it')} />
                     </SwiperSlide>
                     <SwiperSlide>
-                        <img src={SophieBluel} alt='Photo du site Sophie Bluel' onClick={() => openModal(SophieBluel, (
+                        <img src={SophieBluel} alt='Photo du site Sophie Bluel' onClick={() => openModal(ModalSophie, (
                             <p>
-                                <strong>Client :</strong>
+                                <strong>Client : </strong>
                                 Sophie Bluel architecte d'intèrieur.
                                 <br></br>
-                                <strong>Présentation :</strong>
-                                Sophie Bluel souhaite rendre son portfolio dynamique en utilisant JavaScript.
-                                <strong>Objectif :</strong>
-                                Récupérer dynamiquement les données des travaux via une API, Permettre de trier/filtrer les projets par catégorie dans la galerie de photos. Permettre a Sophie Bluel de se connecter pour éditer son site. Créer la modale lui permettant de gérer ses projets : ajout, suppression de projets.
+                                <strong>Présentation : </strong>
+                                Sophie Bluel souhaite rendre son portfolio dynamique en utilisant JavaScript.<br></br>
+                                <strong>Objectif : </strong>
+                                Récupérer dynamiquement les données des travaux via une API, Permettre de trier/filtrer les projets par catégorie dans la galerie de photos. Permettre a Sophie Bluel de se connecter pour éditer son site. Créer la modale lui permettant de gérer ses projets : ajout, suppression de projets.<br></br>
                                 <strong>Pour cela j'ai utilisée: "JavaScript", "Swagger","NodeJS".</strong>
                             </p>
                         ), 'https://github.com/Fannydev974/P06-Sophie-Bluel')} />
                     </SwiperSlide>
                     <SwiperSlide>
-                        <img src={ArgentBank} alt='Photo du site ArgentBank' onClick={() => openModal(ArgentBank, (
+                        <img src={ArgentBank} alt='Photo du site ArgentBank' onClick={() => openModal(ModalArgent, (
                             <p>
-                                <strong>Client :</strong>
+                                <strong>Client : </strong>
                                 ArgentBank, Nouveau sur le marché des banque en ligne
                                 <br></br>
-                                <strong>Présentation :</strong>
-                                ArgentBank est une nouvelle banque en ligne qui souhaite percer dans le secteur bancaire.
-                                <strong>Objectif :</strong>
+                                <strong>Présentation : </strong>
+                                ArgentBank est une nouvelle banque en ligne qui souhaite percer dans le secteur bancaire.<br></br>
+                                <strong>Objectif : </strong>
                                 Créer l’application web complète et responsive avec React.Responsable de la configuration du tableau de bord utilisateur pour notre nouvelle application web React chez ArgentBank.
-                                Collaboration avec Mila, la cheffe de projet, pour répondre aux exigences fixées par Avery et terminer les tâches dans les délais impartis.
+                                Collaboration avec Mila, la cheffe de projet, pour répondre aux exigences fixées par le client et terminer les tâches dans les délais impartis.<br></br>
                                 <strong>Pour cela j'ai utilisée: "React", "Redux", "Green Code", "MongoDB".</strong>
                             </p>
                         ), 'https://github.com/Fannydev974/P11-ArgentBank')} />
                     </SwiperSlide>
                     <SwiperSlide>
-                        <img src={Kasa} alt='Photo du site Kasa' onClick={() => openModal(Kasa, (
+                        <img src={Kasa} alt='Photo du site Kasa' onClick={() => openModal(ModalKasa, (
                             <p>
-                                <strong>Client :</strong>
+                                <strong>Client : </strong>
                                 Kasa,  startup spécialisée dans la location d'appartements entre particuliers.
                                 <br></br>
-                                <strong>Présentation :</strong>
-                                Kasa souhaite faire une refonte totale de leurs site. Mon rôle était de concevoir une plateforme web en utilisant React pour le côté front-end, et Node.js pour le côté backend, ainsi que des maquettes fournies par le designer.
-                                <strong>Objectif :</strong>
-                                Développer l'ensemble de l'application, Utiliser les composants React, Créer les routes avec React Router, Utiliser Sass pour le design, Site responsive.
+                                <strong>Présentation : </strong>
+                                Kasa souhaite faire une refonte totale de leurs site. Mon rôle était de concevoir une plateforme web en utilisant React pour le côté front-end, et Node.js pour le côté backend, ainsi que des maquettes fournies par le designer.<br></br>
+                                <strong>Objectif : </strong>
+                                Développer l'ensemble de l'application, Utiliser les composants React, Créer les routes avec React Router, Utiliser Sass pour le design, Site responsive.<br></br>
                                 <strong>Pour cela j'ai utilisée: "React", "Sass", "Figma".</strong>
                             </p>
                         ), 'https://github.com/Fannydev974/P7-Kasa',)} />
                     </SwiperSlide>
 
                     <SwiperSlide>
-                        <img src={NinaCarducci} alt='Photo du site Nina Carducci' onClick={() => openModal(NinaCarducci, (
+                        <img src={NinaCarducci} alt='Photo du site Nina Carducci' onClick={() => openModal(ModalNina, (
                             <p>
-                                <strong>Client :</strong>
+                                <strong>Client : </strong>
                                 Nina Carducci, photographe professionnelle.
                                 <br></br>
-                                <strong>Présentation :</strong>
-                                Nina Carducci, par le biais de son site souhaite présenter son portfolio pour mettre en valeur ses réalisations. Elle souhaite également améliorer le référencement naturel (SEO) de son site.
-                                <strong>Objectif :</strong>
-                                Ma mission visait à améliorer la performance globale du site en optimisant les images du site trop lourdes qui rendent la page très longue à charger, Optimiser le code et sa structure afin d'améliorer le chargement et l'accessibilité du site, Produire un rapport d'optimisation présentant les améliorations mises en place.
+                                <strong>Présentation : </strong>
+                                Nina Carducci, par le biais de son site souhaite présenter son portfolio pour mettre en valeur ses réalisations. Elle souhaite également améliorer le référencement naturel (SEO) de son site.<br></br>
+                                <strong>Objectif : </strong>
+                                Ma mission visait à améliorer la performance globale du site en optimisant les images du site trop lourdes qui rendent la page très longue à charger, Optimiser le code et sa structure afin d'améliorer le chargement et l'accessibilité du site, Produire un rapport d'optimisation présentant les améliorations mises en place.<br></br>
                                 <strong>Pour cela j'ai utilisée: "SEO", "Lighthouse", "Wave".</strong>
                             </p>
                         ), 'https://github.com/Fannydev974/P9-Nina-Carducci',)} />
                     </SwiperSlide>
 
                     <SwiperSlide>
-                        <img src={Events} alt='Photo du site Events' onClick={() => openModal(Events, (
+                        <img src={Events} alt='Photo du site Events' onClick={() => openModal(Modal724, (
                             <p>
-                                <strong>Client :</strong>
+                                <strong>Client : </strong>
                                 Agence évènementielle 724events.
                                 <br></br>
-                                <strong>Présentation :</strong>
-                                724events, une agence événementielle, a fait une refonte de son site vitrine. Il on fait appel a moi pour résoudre les problèmes techniques et optimiser le site pour répondre pleinement aux besoins de l'agence.
-                                <strong>Objectif :</strong>
-                                Effectuer un débogage complet et optimiser le site web de 724events, plus précisement Réparer le carrousel en affichant les évènements par date en ordre décroissant. Réaliser le filtrage de "Nos réalisations". Afficher les bons mois des réalisations de "Nos réalisations". Corriger le formulaire, et afficher un message de confirmation. Et Ajouter le dernier évènement au footer avec l'image et la date.
+                                <strong>Présentation : </strong>
+                                724events, une agence événementielle, a fait une refonte de son site vitrine. Il on fait appel a moi pour résoudre les problèmes techniques et optimiser le site pour répondre pleinement aux besoins de l'agence.<br></br>
+                                <strong>Objectif : </strong>
+                                Effectuer un débogage complet et optimiser le site web de 724events, plus précisement Réparer le carrousel en affichant les évènements par date en ordre décroissant. Réaliser le filtrage de "Nos réalisations". Afficher les bons mois des réalisations de "Nos réalisations". Corriger le formulaire, et afficher un message de confirmation. Et Ajouter le dernier évènement au footer avec l'image et la date.<br></br>
                                 <strong>Pour cela j'ai utilisée: "Débogage", "Test unitaire", "Optimisation".</strong>
                             </p>
                         ), 'https://github.com/Fannydev974/P10-724Events',)} />
@@ -237,20 +246,32 @@ function Portfolio() {
                 )}
             </div>
 
+
+
             {modalOpen && (
                 <div className="modal-overlay" onClick={closeModal}>
                     <div className="modal" onClick={(event) => event.stopPropagation()}>
-
-                        <ModalContent
-                            description={selectedDescription}
-                            githubLink={selectedGithubLink}
-
-                        />
+                        <div className="modal-content">
+                            <div className="modal-left">
+                                {/* Utilisation de l'image sélectionnée dynamiquement */}
+                                <img src={selectedImage} alt="Aperçu du projet" />
+                            </div>
+                            <div className="modal-right">
+                                <ModalContent
+                                    description={selectedDescription}
+                                    githubLink={selectedGithubLink}
+                                />
+                            </div>
+                        </div>
                         <button className="close-button" onClick={closeModal}>X</button>
                     </div>
                 </div>
             )}
         </div>
+
+
+
+
     );
 }
 
